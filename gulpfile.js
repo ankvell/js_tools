3db17cb2fda9cf119a7a247fdb6b96f1da7393a7
ankvell <an.kvellia@gmail.com>
2015-07-13 15:21:40 +0300

    Added sourcemaps. Minified css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 // Directories
 var sassDir = 'dev/styles';
@@ -13,14 +17,28 @@ var targetJsDir = './dist/scripts';
 
 gulp.task('sassCompile', function() {
     return gulp.src(sassDir + '/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(targetCssDir))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifyCss())
         .pipe(gulp.dest(targetCssDir));
 });
 
 gulp.task('scripts', function() {
     return gulp.src(jsDir + '/*.js')
+        .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(targetJsDir))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(targetJsDir));
 });
 
 
